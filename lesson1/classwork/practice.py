f9f9465297acd7ed8e3202d197a2f62c89a862a9
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-08 21:32:22 +0000

    practice

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,37 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
 
+favorite_number = 7
+print(favorite_number)
 
 
 # Problem 2
 # Create a variable for your height in meters and print it.
 
-
+height = 1.72
+print(height)
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
 
-
+favorite_animal = "tiger"
+print("My favorite animal is", favorite_animal)
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
 
-
+first_name = "John"
+last_name = "Doe"
+print(first_name)
+print(last_name)
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+
+first_name = "John"
+age = 13
+favorite_color = "blue"
+
+print("My name is", first_name + ", I am", age, "years old, and my favorite color is", favorite_color + ".")
