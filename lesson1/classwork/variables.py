a26506109f8fde7584c969d48696131836b9c5c2
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-08 21:08:49 +0000

    variables

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,14 @@
+x = 5 # integer (whole number)
+print(x)
+
+number = 3.97 # float (decimal expression)
+print(number)
+
+favorite_food = "pizza" # string (text)
+print("My favorite food is " + favorite_food)
+print("My favorite food is", favorite_food)
+
+cost = 10
+print("This burger costs $" + str(cost))
+
+print(4.21, "dogs", x)
