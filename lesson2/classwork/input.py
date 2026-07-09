fd89f5da535a5d7a4b859e9f9fe9327fdc6e5ab5
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-09 20:45:40 +0000

    input

---
+++ lesson2/classwork/input.py
@@ -0,0 +1,11 @@
+user_input = input("Give me an integer: ")
+print(user_input)
+
+# Error!!!
+# print(user_input + 1)
+
+number = int(user_input)
+print(number + 1)
+
+user_number = int(input("give me a number: "))
+print(user_number + 1)
