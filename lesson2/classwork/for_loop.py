2afe9962ae7640df593b15f49dd388162228a55c
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-09 20:41:10 +0000

    for loop

---
+++ lesson2/classwork/for_loop.py
@@ -0,0 +1,19 @@
+for i in range(4):
+    print("Hello world!")
+    print("Hello world!")
+    print("Hello world!")
+
+for i in range(1 + 2):
+    print("Coding time!")
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i * 2)
+
+for i in range(5):
+    print("The number is", i * 2 + 1)
+
+for asdf in range(13):
+    print(asdf)
