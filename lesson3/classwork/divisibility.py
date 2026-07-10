824b06afcac279d21f1059d56b03f1838f09e233
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-10 20:40:55 +0000

    divisibility

---
+++ lesson3/classwork/divisibility.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+if num % 3 == 0:
+    print("Number is divisible by 3!")
+
+if num % 5 == 0:
+    print("Number is divisible by 5!")
+
+if num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
