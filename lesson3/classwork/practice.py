b05fa9fbb8e6850d9147b5bfc25df18da69d1da1
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-10 21:30:43 +0000

    practice

--- lesson3/classwork/practice.py
+++ lesson3/classwork/practice.py
@@ -2,13 +2,21 @@
 # Ask the user to enter their height in centimeters.
 # Print "Tall" if the height is greater than 170, otherwise print "Short".
 
-
+height = int(input("Enter your height in cm: "))
+if height > 170:
+    print("Tall")
+else:
+    print("Short")
 
 # Problem 2
 # Ask the user for their age.
 # If they are 18 or older, print "Adult", else print "Minor".
 
-
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
 
 # Problem 3
 # Ask the user to enter a number.
@@ -16,16 +24,36 @@
 # print "FizzBuzz" if divisible by both 3 and 5,
 # otherwise print the number itself.
 
-
+num = int(input("Enter a number: "))
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)
 
 # Problem 4
 # Ask for age and height.
 # If age is at least 10 AND height is at least 120 cm, print "You can ride!"
 # Otherwise, print "Sorry, you can't ride."
 
+age = int(input("How old are you? "))
+height = int(input("What is your height in cm? "))
 
+if age >= 10 and height >= 120:
+    print("You can ride!")
+else:
+    print("Sorry, you can't ride")
 
 # Problem 5
 # Ask the user for a number.
 # If it's divisible by 3 AND (either less than 0 OR greater than 100), print "Weird number!"
 # Otherwise, print "Normal number."
+
+num = int(input("Enter a number: "))
+if num % 3 == 0 and (num < 0 or num > 100):
+    print("Weird number!")
+else:
+    print("Normal number.")
