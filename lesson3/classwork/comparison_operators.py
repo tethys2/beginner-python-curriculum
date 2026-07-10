f56d48a069dd8b87c6b7d2db4662b30a3eb7e900
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-07-10 20:14:49 +0000

    comparison operators

---
+++ lesson3/classwork/comparison_operators.py
@@ -0,0 +1,31 @@
+print(5 > 3) # Greater than operator
+print(5 < 3) # Less than operator
+print(5 >= 3) # Greater than or equal to operator
+print(5 >= 5) # Greater than or equal to operator
+print(3 <= 5) # Less than or equal to operator
+print(3 <= 3) # Less than or equal to operator
+print(3 == 3) # Equal to (note: double equals)
+print(5 != 3) # Not equal
+
+a = 10
+b = 7
+
+print(a > b) # Like 10 > 7 ???
+print(a == b)
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print(x > y)
+
+print("apple" == "apple")
+print("Apple" == "apple") # A != B
+print("apple" != "banana")
+print("cat" > "bat") # Compared alphabetically
+print("dog" < "zebra") # Compared alphabetically
+
+
+
+
+
+
+
