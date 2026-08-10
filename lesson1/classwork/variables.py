e69d85b7811b0c09d11d64fb4f3e7e555a631096
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-08-10 21:04:18 +0000

    practice

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,12 @@
+x = 5 # Integer: whole number with a decimal
+print(x)
+
+number = 3.97 # Float: number with a decimal point
+print(number)
+
+favorite_food = "pizza"
+# Each comma adds a space and lets you add a new value to the same line
+print("My favorite food is", favorite_food) 
+
+# Different types in the same strign work just fine
+print(4.21, "dogs", 18)
