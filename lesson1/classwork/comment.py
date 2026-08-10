cecddf5f928c5069c6f833756ebe340de9111f12
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-08-10 19:58:40 +0000

    Reset lesson1

--- lesson1/classwork/comment.py
+++ lesson1/classwork/comment.py
@@ -1,5 +1,1 @@
-print("First message")
-# This is a comment
-# print("Second message")
-print("Third message") # print("Fourth message")
 
--- lesson1/classwork/printing.py
+++
@@ -1,6 +0,0 @@
-print("Hello World")
-print("My name is Toby")
-print('My favorite food is pizza')
-
-# Why error??
-# print("I love dogs')
