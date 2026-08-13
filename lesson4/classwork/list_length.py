15ca92a144ad00a29e253243f9888e39af286097
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-08-13 20:25:14 +0000

    lists length

---
+++ lesson4/classwork/list_length.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry", "date"]
+
+length = len(fruits)
+print("Number of fruits:", length)
+
+last_fruit = fruits[length -  1]
+print("Last fruit:", last_fruit)
+
+# Error: list index out of range
+# print(fruits[length])
