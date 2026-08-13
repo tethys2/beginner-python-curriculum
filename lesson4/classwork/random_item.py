32546d4642d4bcdc5f002f3a3d180fcec9500692
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-08-13 20:34:09 +0000

    random item

---
+++ lesson4/classwork/random_item.py
@@ -0,0 +1,13 @@
+import random
+
+animals = ["cat", "dog", "rabbit", "hamster", "parrot"]
+
+length = len(animals)
+
+random_index = random.randint(0, length - 1)
+
+random_animal = animals[random_index]
+print("Random animal:", random_animal)
+
+shortcut_animal = random.choice(animals)
+print("Random choice shortcut:", shortcut_animal)
