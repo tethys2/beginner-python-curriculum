ff1f8b8df0451ca5038b3f0e4a2e6a890714b714
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-08-13 20:50:53 +0000

    looping lists

---
+++ lesson4/classwork/looping_lists.py
@@ -0,0 +1,9 @@
+numbers = [4, 7, 12, 3]
+
+for i in range(len(numbers)):
+    print(numbers[i])
+
+animals = ["cat", "dog", "rabbit"]
+
+for i in range(len(animals)):
+    print("Animal", i, "is", animals[i])
