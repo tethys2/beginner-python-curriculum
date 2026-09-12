6c79044dce1b24fef5b943619b4c2be9c6af321e
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-09-12 23:54:08 +0000

    reset lesson 1

--- lesson1/classwork/comment.py
+++
@@ -1,3 +0,0 @@
-# This is a comment.
-# We use these to document our code and expain what it is doing!
-# This is an example change
--- lesson1/classwork/printing.py
+++
@@ -1,6 +0,0 @@
-print("Hello world!")
-print('My name is Toby!')
-print('My favorite food is pizza!')
-
-# Error: quotation marks are different
-#print("I love dogs!')
