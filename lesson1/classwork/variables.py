94a9e9e18e9a93e9262dc0ca9f19c0b7eb9485b1
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-09-13 01:26:14 +0000

    variables

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,13 @@
+x = 5 # Integer
+print(x)
+
+number = 3.97 # Float
+print(number)
+
+favorite_food = "pizza" #String
+
+# Comma = space
+# + = no space
+print("My favorite food is", favorite_food + ".")
+
+print(4.21, "dogs", 18)
