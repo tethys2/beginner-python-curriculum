7a769acc87b243b288a88105b566d11193176ef8
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-09-20 00:24:26 +0000

    math operators

--- lesson2/classwork/math_operators.py
+++ lesson2/classwork/math_operators.py
@@ -1,2 +1,26 @@
+print(4 + 5) # Addition: adds two numbers
 
+print(10 - 3) # Subtraction: subtracts the second number from the first number
 
+print(10 * 7) # Multiplication: multiplies two numbers
+
+print(15 / 2) # Division: divides and gives float result
+
+print(15 // 2) # Floor Division: divides and rounds down to teh nearest whole number
+
+print(15 % 2) # Modulus: gives the remainder
+
+print(2 ** 3) # Exponentiation: first number raised to the power of the second number
+
+print(2 + 3 * 4) # print 14 (3 * 4 = 12, then 2 + 12 = 14)
+print((2 + 3) * 4) # print 20 (2 + 3 = 5, then 5 * 4 = 20)
+
+a = 8
+b = 3
+print(a + b) # 11
+print(a * b) # 24
+
+x = 12
+y = 5
+print("Quotient:", x // y)
+print("Remainder:", x % y)
