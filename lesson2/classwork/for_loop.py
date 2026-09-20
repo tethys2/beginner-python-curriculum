2b52550222a5ebfb2df5324fdf0953a9a452b39a
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-09-20 01:19:29 +0000

    for loop

---
+++ lesson2/classwork/for_loop.py
@@ -0,0 +1,15 @@
+for i in range(4): # range(4) = 0, 1, 2, 3
+    print("Hello world!")
+
+for i in range(1+2): # range(1+2) = 0, 1, 2
+    print("Moo!")
+
+num = 6
+for i in range(num): # range(num) = 0, 1 ... num - 1:
+    print("Bark!")
+
+for i in range(5): # 0 .. 4
+    print(i)
+
+for i in range(5):
+    print(i * 2)
