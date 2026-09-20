26670fb3bbfa835f12490d7a20d4a2c42dad8d2a
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-09-20 01:08:13 +0000

    input

---
+++ lesson2/classwork/input.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ")
+print("Hi", name)
+
+year = input("What year is it? ")
+print("It is currently", year)
+
+year_num = int(year)
+print("Next year is", year_num + 1) 
+
+# Error:
+# print("Next year is", year + 1) 
+
+user_number = int(input("Give me a number: "))
+print(user_number + 1)
